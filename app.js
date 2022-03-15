5a28e3e5a5dcc2697fb9df9d5f3875e79d4d08c9
HMS-Victory <90852625+HMS-Victory@users.noreply.github.com>
2022-03-15 08:04:15 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-
-var a="Hell World";
-console.log(a);
-
-
-
-if (a === undefined) {
-    console.log('a is undefined');
-}else{
-    console.log('a is defined!');
+function a(){
+    console.log(this);
+    this.newvariable='hello';
 }
+
+let b=function() {
+    console.log(this);
+}
+
+a();
+
+console.log(newvariable);
+b();
+
+let c={
+    name: 'the c object',
+    log: function() {
+        let self=this;//this works because objects are changed by referance, therfore I will be able to mutate the original this, by changing var self.
+        self.name='Updatd c object';
+        console.log(self);
+
+        let setname=function(newname) {
+            self.name=newname;
+        }
+        setname('Updated again! the c object');
+        console.log(self);
+    }
+}
+
+c.log();
