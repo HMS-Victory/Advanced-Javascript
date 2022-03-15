f4464274d5c13a9b220b2773af4acefae5091bdc
HMS-Victory <90852625+HMS-Victory@users.noreply.github.com>
2022-03-15 08:07:55 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-
-var a="Hell World";
-console.log(a);
-
-
-
-if (a === undefined) {
-    console.log('a is undefined');
-}else{
-    console.log('a is defined!');
+function getPerson() {
+    return {
+        firstname: 'Tony'
+    };
 }
+
+console.log(getPerson());
