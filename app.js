febdab3b5178a41f0d1214c5db6181750c68bb93
HMS-Victory <90852625+HMS-Victory@users.noreply.github.com>
2022-03-15 08:29:29 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+//gets a new object(the architecture allows us to not have to use the ''new
+//keyword here)
+var g = G$('John', 'Doe');
 
-var a="Hell World";
-console.log(a);
+//use our chainable methods
+g.greet().setLang('es').greet(true).log();
 
+//let's use our object on the click of the login button
+$('#login').click(function() {
 
+    //create a new 'Greeter' object (let's pretend we know the name frome the login)
+    let loginGrtr=G$('John', 'Doe');
 
-if (a === undefined) {
-    console.log('a is undefined');
-}else{
-    console.log('a is defined!');
-}
+    //hide the login on the screen
+    $('#logindiv').hide();
+
+    //fire off an HTML greeting, passing the '#greeting' as the selector and
+    //the chose language, and log the welcome as well.
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
