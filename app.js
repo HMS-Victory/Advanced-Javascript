43af69d7eb5d012ff475423e93e618dc1d8cd01f
HMS-Victory <90852625+HMS-Victory@users.noreply.github.com>
2022-03-15 08:27:52 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
+let g=G$('John', 'Doe');
 
-var a="Hell World";
-console.log(a);
-
-
-
-if (a === undefined) {
-    console.log('a is undefined');
-}else{
-    console.log('a is defined!');
-}
+g.greet().setLang('es').greet(true);
